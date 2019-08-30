fbc9ab7add2d71ad52866be3a2d3be48b9f877f3
Fiqi Fitransyah <fiqi.i01107@gmail.com>
2019-08-30 10:41:03 +0700

    User model initiation using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'dashboard/index'
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
