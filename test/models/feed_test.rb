c1832d936f7e26f941e3405ac3cfa6ce4ced4a75
Fiqi Fitransyah <fiqi.i01107@gmail.com>
2019-09-04 10:06:09 +0700

    Fixtures & model tests update

--- test/models/feed_test.rb
+++ test/models/feed_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class FeedTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create feed successfull" do
+    user = users(:one)
+    expected_feed_count = Feed.count + 1
+    feed = Feed.new({user_id: user.id, content: "bla bla"})
+    feed.save
+
+    assert_equal expected_feed_count, Feed.count
+    assert_not_nil Feed.find(feed.id)
+  end
+
+  test "create feed unsuccessfull content blank" do
+    user = users(:one)
+    expected_feed_count = Feed.count
+    feed = Feed.new({user_id: user.id})
+    feed.save
+
+    assert_equal expected_feed_count, Feed.count
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create user successfull" do
+    u = User.find_by_email('test@example.com')
+    u.destroy unless u.blank?
+    expected_user_count = User.count + 1
+    u = User.new ({email: 'test@example.com', first_name: 'Test', password: 'test123pass', password_confirmation: 'test123pass'})
+    u.save
+
+    assert_equal expected_user_count, User.count
+    assert_not_nil User.find_by_email('test@example.com')
+  end
+
+  test "create user unsuccessfull no password" do
+    u = User.find_by_email('test2@example.com')
+    u.destroy unless u.blank?
+    expected_user_count = User.count
+    u = User.new ({email: 'test2@example.com', first_name: 'Test'})
+    u.save
+
+    assert_equal expected_user_count, User.count
+    assert_nil User.find_by_email('test2@example.com')
+  end
+
+  test "create user unsuccessfull email exist" do
+    u = User.find_by_email('test2@example.com')
+    u.destroy unless u.blank?
+    expected_user_count = User.count
+    u = User.new ({email: 'test2@example.com', first_name: 'Test'})
+    u.save
+
+    assert_equal expected_user_count, User.count
+    assert_nil User.find_by_email('test2@example.com')
+  end
 end
